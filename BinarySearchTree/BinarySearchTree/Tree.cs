83375dc9b0c06c8f1afc73515e72345dd5e6ba69
HAxan Simon <simon.ohr@live.se>
2018-01-14 21:32:42 +0100

    Initial Commit

---
+++ BinarySearchTree/BinarySearchTree/Tree.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class Tree
+    {
+        Node node = new Node();
+
+        public Tree()
+        {
+
+        }
+    }
+}
